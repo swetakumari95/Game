62ec069dd6c9c52bd4957f31eaf14ef67ff8e137
Sweta Kumari <sweta191995@gmail.com>
2016-10-08 14:36:36 +0530

    Colission and score added

--- js/script.js
+++ js/script.js
@@ -1,41 +1,114 @@
 $(document).ready(function(){
     
-    //getting intial position of source
-    var $initialSrc = $("#source").position();
-    
+    //calls main function
+    main();
     
     //set random position for target
-    $("#target").css({
-        'left': Math.random()* 90 + '%',
-        'bottom': Math.random()* 80 + '%'
-    });
+    function randomTargetPosition(){
+        $("#target").css({
+            'left': Math.random()* 90 + '%',
+            'bottom': Math.random()* 80 + '%'
+        });
+    };
     
-    //calculates target's position
-    var $targetPosn = $("#target").position();
-    
-    //finds current source position
-    var $sourcePosn = $("#source").position();
-    
-    //moves source up, down, left and right on key press
-    $(document).keydown(function(event){
+    function detectKeyPress($targetPosn, event){
         
-        var key = event.which;
-        if (key==37){
-            //left key
-            $("#source").animate({left: '-=10px'},50);
-        }
-        if (key==38){
-            //up key
-            $("#source").animate({top: '-=10px'},50);
-        }
-        if (key==39){
-            //right key
-            $("#source").animate({left: '+=10px'},50);
-        }
-        if (key==40){
-            //down key
-            $("#source").animate({top: '+=10px'},50);
-        }
+        //moves source up, down, left and right on key press
+        $(document).keydown(function(event){
+            
+            var key = event.which;
+            if (key==37){
+                //left key
+                $("#source").animate(
+                    {left: '-=10px'},
+                    {duration : 50,
+                    progress: function(){
+                        //checks for colission in each and every frame of movement
+                        var $sourcePosn = $("#source").position();
+                        if (Math.abs($targetPosn.top-$sourcePosn.top)<=50 && Math.abs($targetPosn.left-$sourcePosn.left)<=50){
+                            $('#target').hide('explode',2000);
+                            //increase score
+                            score += 1;
+                            $('#score').html("SCORE :"+ score);
+                            main();
+                        }
+                    }}
+                );
+            }
+            if (key==38){
+                //up key
+                $("#source").animate(
+                    {top: '-=10px'},
+                    {duration : 50,
+                    progress: function(){
+                        //checks for colission in each and every frame of movement
+                        var $sourcePosn = $("#source").position();
+                        if (Math.abs($targetPosn.top-$sourcePosn.top)<=50 && Math.abs($targetPosn.left-$sourcePosn.left)<=50){
+                            $('#target').hide('explode',2000);
+                            //increase score
+                            score += 1;
+                            $('#score').html("SCORE :"+ score);
+                            main();
+                        }
+                    }}
+                );
+            }
+            if (key==39){
+                //right key
+                $("#source").animate(
+                    {left: '+=10px'},
+                    {duration : 50,
+                    progress: function(){
+                        //checks for colission in each and every frame of movement
+                        var $sourcePosn = $("#source").position();
+                        if (Math.abs($targetPosn.top-$sourcePosn.top)<=50 && Math.abs($targetPosn.left-$sourcePosn.left)<=50){
+                            $('#target').hide('explode',2000);
+                            //increase score
+                            score += 1;
+                            $('#score').html("SCORE :"+ score);
+                            main();
+                        }
+                    }}
+                );
+            }
+            if (key==40){
+                //down key
+                $("#source").animate(
+                    {top: '+=10px'},
+                    {duration : 50,
+                    progress: function(){
+                        //checks for colission in each and every frame of movement
+                        var $sourcePosn = $("#source").position();
+                        if (Math.abs($targetPosn.top-$sourcePosn.top)<=50 && Math.abs($targetPosn.left-$sourcePosn.left)<=50){
+                            $('#target').hide('explode',2000).remove();
+                            //increase score
+                            score += 1;
+                            $('#score').html("SCORE :"+ score);
+                            main();
+                        }
+                    }}
+                );
+            }
+        });
+    };
+    
+    
+    //getting intial position of source
+    var $initialSrc = $("#source").position();
+    var score = 0;
+    
+    function main(){
+    
+        randomTargetPosition();
         
-    });
+        $("target").show();
+        //calculates target's position
+        var $targetPosn = $("#target").position();
+
+        //finds current source position
+        var $sourcePosn = $("#source").position();
+
+        detectKeyPress($targetPosn, event);   
+        
+    }
 });
